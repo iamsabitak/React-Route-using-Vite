8888921393810744359c152056251703098e7b90
sabita <sabitakhadka091@gmail.com>
2024-03-04 21:11:01 +0545

    Integrate UserData component into App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 // import CreateUser from "./components/CreateUser";
 
 // import AppContent from "./AppContent";
-import UserData from "./UserData";
+import UserData from "./userdata/UserData";
 
 function App() {
   return (
