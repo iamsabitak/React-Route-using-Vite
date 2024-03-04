e03f44f88f5f2de8372d3ab250eb35926d3c7466
sabita <sabitakhadka091@gmail.com>
2024-03-04 13:55:22 +0545

    imported AppContent to app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { BrowserRouter as Router } from "react-router-dom";
+
 import AppContent from "./AppContent";
-// import CounterRedux from "./CounterRedux";
 
 function App() {
   return (
     <Router>
-      {" "}
       <AppContent />
-      {/* <CounterRedux /> */}
     </Router>
   );
 }
