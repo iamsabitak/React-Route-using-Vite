0ba7b0117291b5ee6e78230ddb8daa64e7780a40
sabita <sabitakhadka091@gmail.com>
2024-03-04 13:54:24 +0545

    added some default contentto about component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,35 @@
 function About() {
-  return <div>About</div>;
+  return (
+    <div className="container-fluid py-5 about-background">
+      <div className="container">
+        <h2 className="text-center mb-5">About Us</h2>
+        <div className="row">
+          <div className="col-md-6">
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOBIzsyULZ3ZaZ2c9DgZb7EAgwiDhIUftt1zIFboVJgA&s"
+              alt="About Us"
+              className="img-fluid rounded"
+            />
+          </div>
+          <div className="col-md-6 d-flex align-items-center">
+            <div>
+              <p className="lead">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut
+                arcu nisi. Curabitur ac leo eu libero elementum convallis nec
+                eget velit. Proin consequat eros vel felis mattis, nec
+                consectetur mauris finibus.
+              </p>
+              <p>
+                Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
+                posuere cubilia curae; In aliquet turpis a urna maximus, nec
+                elementum velit luctus. Maecenas nec nunc mi.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default About;
