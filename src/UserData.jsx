619a8ed59c2144c3e58fdad2013885bfd5d6f2b1
sabita <sabitakhadka091@gmail.com>
2024-03-04 20:42:37 +0545

    Add UserData component to display and fetch user data

--- src/UserData.jsx
+++ src/UserData.jsx
@@ -1,12 +1,16 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchData } from "./features/dataSlice";
 
 function UserData() {
-  const Users = useSelector((state) => state.userData.data);
+  const user = useSelector((state) => state.data);
+  const dispatch = useDispatch();
+
   return (
     <div>
-      {Users.map((user) => (
-        <li key={user.id}>{user.email}</li>
+      {user.data.map((user) => (
+        <li key={user.id}>{user.name}</li>
       ))}
+      <button onClick={() => dispatch(fetchData())}>Fetch User</button>
     </div>
   );
 }
