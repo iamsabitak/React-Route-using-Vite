2b9c45fd198bd993daf290e25464fb83e9d3ab99
sabita <sabitakhadka091@gmail.com>
2024-03-04 16:31:48 +0545

    imported userdetailreducer in store.js

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "../features/counterSlice";
 import todoReducer from "../features/todoSlice";
+import userDetailReducer from "../features/userDetailSlice";
 
 const store = configureStore({
   reducer: {
     counter: counterReducer,
     todo: todoReducer,
+    userDetails: userDetailReducer,
   },
 });
 
