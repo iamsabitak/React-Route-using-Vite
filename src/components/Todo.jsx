c8edc77004ad8e6c87438201a79c1b75ba41718e
sabita <sabitakhadka091@gmail.com>
2024-03-04 12:08:15 +0545

    created a todo component to remove todo

---
+++ src/components/Todo.jsx
@@ -0,0 +1,31 @@
+import { useDispatch, useSelector } from "react-redux";
+import { removeTodo } from "../features/todoSlice";
+
+function Todos() {
+  const todos = useSelector((state) => state.todo.todos);
+  const dispatch = useDispatch();
+
+  return (
+    <div className="container mt-3">
+      <h2>Todo List</h2>
+      <ul className="list-group">
+        {todos.map((todo) => (
+          <li
+            key={todo.id}
+            className="list-group-item d-flex justify-content-between align-items-center"
+          >
+            {todo.text}
+            <button
+              className="btn btn-danger"
+              onClick={() => dispatch(removeTodo(todo.id))}
+            >
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Todos;
