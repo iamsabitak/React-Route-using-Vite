3712eb5f49e7e79d25315d6f65187457cf2de487
sabita <sabitakhadka091@gmail.com>
2024-03-04 17:15:41 +0545

    created its own different file

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { BrowserRouter as Router } from "react-router-dom";
+// import CreateUser from "./components/CreateUser";
 
 import AppContent from "./AppContent";
+// import ShowUser from "./components/ShowUser";
 
 function App() {
   return (
     <Router>
       <AppContent />
+      {/* <ShowUser /> */}
+      {/* <CreateUser /> */}
     </Router>
   );
 }
