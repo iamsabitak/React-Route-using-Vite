0e90cbece481771f329fec9da7735e8efe6b4e5e
sabita <sabitakhadka091@gmail.com>
2024-03-04 21:10:07 +0545

    Remove unused userDetailSlice from Redux store configuration

--- src/app/store.js
+++ src/app/store.js
@@ -1,14 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
+
 import counterReducer from "../features/counterSlice";
 import todoReducer from "../features/todoSlice";
-import userDetailReducer from "../features/userDetailSlice";
 import dataSlice from "../features/dataSlice";
 
 const store = configureStore({
   reducer: {
     counter: counterReducer,
     todo: todoReducer,
-    userDetails: userDetailReducer,
+
     data: dataSlice,
   },
 });
