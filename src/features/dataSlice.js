87bdc60853f10129715199d415b30f8ee5a66acf
sabita <sabitakhadka091@gmail.com>
2024-03-04 17:16:43 +0545

    created dataSlice component

---
+++ src/features/dataSlice.js
@@ -0,0 +1,36 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const fetchData = createAsyncThunk("data/fetchData", async () => {
+  const response = await axios.get(
+    "https://jsonplaceholder.typicode.com/users"
+  );
+  console.log(response.data);
+  return response.data;
+});
+
+const dataSlice = createSlice({
+  name: "data",
+  initialState: {
+    data: [],
+    loading: false,
+    error: null,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchData.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(fetchData.fulfilled, (state, action) => {
+        state.loading = false;
+        state.data = action.payload;
+      })
+      .addCase(fetchData.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      });
+  },
+});
+
+export default dataSlice.extraReducers;
